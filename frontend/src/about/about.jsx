7cdb8fcd82e51a84a6d7c5983f4c1f9dc4c5e5f6
Vitor Cassiano <virjcas@gmail.com>
2017-12-19 00:33:24 -0200

    WiP - Melhorando a pagina de todo, criando formulario e grid

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -5,10 +5,10 @@ export default props => (
     <div>
         <PageHeader name='Sobre' small='Nós'></PageHeader>
         <h2>Nossa História</h2>
-        <p>lsbalsbdlasbdlasbdlasbdlasbdlasdbasldb</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia ultrices libero, non fermentum erat placerat a. Ut efficitur sagittis fermentum. Aenean convallis varius orci, sed aliquam risus consequat non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eros mi, tempus vel sollicitudin at, bibendum sed eros. Aenean ullamcorper eget sem feugiat aliquet. Aenean nec placerat nisi. Phasellus maximus purus vitae odio sollicitudin pharetra. Ut eleifend enim sit amet ante porta, vel lobortis dui pulvinar. Nunc vehicula posuere nisl, in rhoncus purus aliquet a. Nulla molestie feugiat ipsum et congue. Quisque hendrerit rhoncus mi, nec malesuada quam aliquam at.</p>
         <h2>Missão e Visão</h2>
-        <p>lsbalsbdlasbdlasbdlasbdlasbdlasdbasldb</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia ultrices libero, non fermentum erat placerat a. Ut efficitur sagittis fermentum. Aenean convallis varius orci, sed aliquam risus consequat non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eros mi, tempus vel sollicitudin at, bibendum sed eros. Aenean ullamcorper eget sem feugiat aliquet. Aenean nec placerat nisi. Phasellus maximus purus vitae odio sollicitudin pharetra. Ut eleifend enim sit amet ante porta, vel lobortis dui pulvinar. Nunc vehicula posuere nisl, in rhoncus purus aliquet a. Nulla molestie feugiat ipsum et congue. Quisque hendrerit rhoncus mi, nec malesuada quam aliquam at.</p>
         <h2>Imprensa</h2>
-        <p>lsbalsbdlasbdlasbdlasbdlasbdlasdbasldb</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia ultrices libero, non fermentum erat placerat a. Ut efficitur sagittis fermentum. Aenean convallis varius orci, sed aliquam risus consequat non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eros mi, tempus vel sollicitudin at, bibendum sed eros. Aenean ullamcorper eget sem feugiat aliquet. Aenean nec placerat nisi. Phasellus maximus purus vitae odio sollicitudin pharetra. Ut eleifend enim sit amet ante porta, vel lobortis dui pulvinar. Nunc vehicula posuere nisl, in rhoncus purus aliquet a. Nulla molestie feugiat ipsum et congue. Quisque hendrerit rhoncus mi, nec malesuada quam aliquam at.</p>
     </div>
 )
---
+++ frontend/src/todo/todoList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+    <div>
+        <h1>Foobar</h1>
+    </div>
+)
