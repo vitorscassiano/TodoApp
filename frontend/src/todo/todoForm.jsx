939080f49fdf11882626ab77679d5367edd1bab0
Vitor Cassiano <virjcas@gmail.com>
2017-12-20 00:05:17 -0200

    WiP - Add behavior on component description

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -6,11 +6,11 @@ export default props => (
     <div>
         <div role='form' className='todoForm'>
             <Grid cols='12 9 10'>
-                <input id='description' className='form-control' placeholder='Adicione uma tarefa...' />
+                <input id='description' value={props.description} onChange={props.handleChange} className='form-control' placeholder='Adicione uma tarefa...' />
             </Grid>
 
             <Grid cols='12 3 2'>
-                <IconButton hide={false} style='primary' icon='plus' />
+                <IconButton hide={false} onClick={props.handleAdd} style='primary' icon='plus' />
             </Grid>
         </div>
     </div>
